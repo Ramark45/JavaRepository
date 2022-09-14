ef6856da91325233364f1f1e99a81273c71abe44
Ramark45 <ramarsince2001@gmail.com>
2022-09-14 12:26:33 +0530

    New file added

---
+++ src/PrintingEvenNumbers.java
@@ -0,0 +1,9 @@
+public class PrintingEvenNumbers {
+    public static void main(String[] args) {
+        int i;
+        for(i=1 ; i<=20 ;i++){
+            if(i % 2 == 0)
+                System.out.print(i+ " ");
+        }
+    }
+}
