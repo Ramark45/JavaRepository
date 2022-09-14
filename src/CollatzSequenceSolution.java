a1c1f0f1b0ce512b79ecf699a59c94b2a95539d0
Ramark45 <ramarsince2001@gmail.com>
2022-09-14 14:32:57 +0530

    Collatz Sequence Solution

--- src/CollatzSequenceSolution.java
+++ src/CollatzSequenceSolution.java
@@ -1,2 +1,24 @@
+import java.util.Scanner;
+
 public class CollatzSequenceSolution {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter any number : ");
+        int n = input.nextInt();
+        if (n == 1) {
+            System.out.println(n+ "\n"+"0");
+        }
+        else {
+            System.out.println(n);
+            while (n > 1) {
+                if (n % 2 == 0)
+                {
+                    n = n / 2;
+                } else {
+                    n = (3 * n) + 1;
+                }
+                System.out.println(n);
+            }
+        }
+    }
 }
