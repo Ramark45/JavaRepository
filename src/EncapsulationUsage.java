8eb095a3d1b961f9acd0720a343e9236b57b5497
Ramark45 <113004956+Ramark45@users.noreply.github.com>
2022-09-19 18:56:55 +0530

    Delete EncapsulationUsage.java

--- src/EncapsulationUsage.java
+++
@@ -1,7 +0,0 @@
-//public class EncapsulationUsage {
-//    public static void main(String[] args) {
-//        EncapsulationUsage eu = new EncapsulationUsage();
-//        System.out.println(eu.getName());
-//
-//    }
-//}
