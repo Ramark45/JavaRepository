e53ab8b71fd19dcdfb03df926a1b4e62328f3aa7
Ramark45 <ramarsince2001@gmail.com>
2022-09-22 00:00:46 +0530

    New file

--- src/UpperBoundWildcard.java
+++ src/UpperBoundWildcard.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class UpperBoundWildcard {
+import java.util.ArrayList;
+import java.util.List;
+public class UpperBoundWildcard {
+        public static void print(List<? extends Number> AllElements)
+        {
+            System.out.println(AllElements);
+        }
+
+        public static void main(String[] args) {
+            List<Integer> il=new ArrayList<>();
+            il.add(16);
+            il.add(18);
+            print(il);
+            List<Double> dl=new ArrayList<>();
+            dl.add(1.5);
+            dl.add(2.5);
+            dl.add(3.5);
+            print(dl);
+        }
 }
