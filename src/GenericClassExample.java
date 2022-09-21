c2b45f43e1d8c6dbf5fd06590a2c8442d6affad9
Ramark45 <ramarsince2001@gmail.com>
2022-09-22 00:06:08 +0530

    New file

--- src/GenericClassExample.java
+++ src/GenericClassExample.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class GenericClassExample {
-}
+public class GenericClassExample <generic>{
+        generic g;
+        public GenericClassExample(generic g){
+            this.g=g;
+        }
+        public generic getG(){
+            return g;
+        }
+
+        public static void main(String[] args) {
+            GenericClassExample<String> gen1 = new GenericClassExample<String>("This is Generic method");
+            GenericClassExample<Integer>gen2 = new GenericClassExample<Integer>(100);
+            System.out.println(gen1.getG());
+            System.out.println(gen2.getG());
+        }
+    }
