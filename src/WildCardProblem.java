570a258f9ffed875b506166ef2abe2e8c79589b6
Ramark45 <ramarsince2001@gmail.com>
2022-09-22 00:02:38 +0530

    New file

--- src/WildCardProblem.java
+++ src/WildCardProblem.java
@@ -1,2 +1,21 @@
-package PACKAGE_NAME;public class WildCardProblem {
+import java.util.ArrayList;
+import java.util.List;
+
+public class WildCardProblem {
+    public static void print(List<?> AllElements)
+    {
+        System.out.println(AllElements);
+    }
+
+    public static void main(String[] args) {
+        List<Integer> il=new ArrayList<>();
+        il.add(10);
+        il.add(20);
+        print(il);
+        List<Double> dl=new ArrayList<>();
+        dl.add(1.5);
+        dl.add(2.5);
+        dl.add(3.5);
+        print(dl);
+    }
 }
